3d92e4e860e9f5936299e55f6b75eb391fe00120
Blockzilla101 <blockzilla101@gmail.com>
2022-12-12 17:30:02 +0500

    wip api/access-control

---
+++ src/api/access-control.ts
@@ -0,0 +1,23 @@
+import { ApiClient } from "./client.ts";
+
+export class AccessControl {
+    constructor(public client: ApiClient) {}
+
+    public listBlacklisted() {
+    }
+
+    public listWhitelisted() {
+    }
+
+    public enable() {
+    }
+
+    public disable() {
+    }
+
+    public add() {
+    }
+
+    public remove() {
+    }
+}
